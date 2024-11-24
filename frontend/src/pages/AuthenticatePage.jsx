2abf8399cef48d26f25f7e70f0aff511dd111cd7
Denis Lorenzo Neagoe <gamelover.gamer@yahoo.com>
2024-11-24 19:46:34 +0100

    feat: fixed navbar redirect and login css

--- frontend/src/pages/AuthenticatePage.jsx
+++ frontend/src/pages/AuthenticatePage.jsx
@@ -1,16 +1,63 @@
-import NavBar from "@/components/NavBar"
-import React from "react"
-import {Button} from "@mui/material";
+import { Button, Box, Typography, Paper } from "@mui/material"
+import NavBar from "../components/NavBar.jsx"
+import login from '@/assets/pictures/login.svg'
 
-const AuthenticatePage = () => {
-    return(
+const LoginPage = () => {
+    const handleLogin = () => {
+        window.location.href = "http://localhost:8080/oauth2/authorization/google";
+    }
+
+    return (
         <>
             <NavBar />
-            <Button variant="contained" onClick={() => window.location.href="http://localhost:8080/oauth2/authorization/google"}>
-                Login with Google
-            </Button>
+            <Box
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    minHeight: "100vh",
+                    backgroundColor: "#f5f5f5",
+                }}
+            >
+                <Paper
+                    elevation={3}
+                    sx={{
+                        padding: 4,
+                        borderRadius: 2,
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "20px",
+                        width: "100%",
+                        maxWidth: 400,
+                    }}
+                >
+                    <div style={{display: "flex", justifyContent: "center"}}>
+                        <img src={login} alt="Authenticate" style={{width: "200px"}} />
+                    </div>
+                    <Typography variant="h4" align="center" gutterBottom className="hero-title">
+                        Login to MindEase
+                    </Typography>
+                    <Box
+                        component="form"
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            gap: 2,
+                        }}
+                    >
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            onClick={handleLogin}
+                        >
+                            Login with Google
+                        </Button>
+                    </Box>
+                </Paper>
+            </Box>
         </>
     )
 }
 
-export default AuthenticatePage
+export default LoginPage
