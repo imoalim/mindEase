f5597fb1632917ef1cbe53e7a205cc4d0aeaf6de
Denis Lorenzo Neagoe <gamelover.gamer@yahoo.com>
2024-11-26 15:51:08 +0100

    fix: fixed setting verification step after questionnaire

--- backend/src/main/java/com/example/mindEase/service/UserService.java
+++ backend/src/main/java/com/example/mindEase/service/UserService.java
@@ -39,25 +39,25 @@ public class UserService {
     public Long saveUserEmotionalStateQuestionnaire(UserQuestionnaireRequest request){
         User user = this.findUserById(request.getUserId())
                 .orElseThrow(() -> new IllegalArgumentException("User not found for ID: " + request.getUserId()));
-            UserQuestionnaire userQuestionnaire = UserQuestionnaire.builder()
-                    .q2(request.getQ2())
-                .q20(request.getQ20())
-                .q22(request.getQ22())
-                .q23(request.getQ23())
-                .q24(request.getQ24())
-                .q25(request.getQ25())
-                .q26(request.getQ26())
-                .q29(request.getQ29())
-                .q30(request.getQ30())
-                .q31(request.getQ31())
-                .q32(request.getQ32())
-                    .user(user)
-                    .build();
+        UserQuestionnaire userQuestionnaire = UserQuestionnaire.builder()
+                .q2(request.getQ2())
+            .q20(request.getQ20())
+            .q22(request.getQ22())
+            .q23(request.getQ23())
+            .q24(request.getQ24())
+            .q25(request.getQ25())
+            .q26(request.getQ26())
+            .q29(request.getQ29())
+            .q30(request.getQ30())
+            .q31(request.getQ31())
+            .q32(request.getQ32())
+                .user(user)
+                .build();
 
-            UserQuestionnaire savedQuestionnaire = userQuestionnaireRepository.save(userQuestionnaire);
-            return savedQuestionnaire.getId();
+        user.setVerificationStep(3);
+        userRepository.save(user);
 
-
+        UserQuestionnaire savedQuestionnaire = userQuestionnaireRepository.save(userQuestionnaire);
+        return savedQuestionnaire.getId();
     }
-
 }
